cdc091ed69201498e89b97e5031185dddb34b1cf
FERGUI SAMY <samy.fergui@etu.unistra.fr>
2024-03-06 14:45:31 +0100

    impl tests

--- test_carte_pizzeria.py
+++ test_carte_pizzeria.py
@@ -1,11 +1,32 @@
 import unittest
 from carte_pizzeria import CartePizzeria
+from carte_pizzeria_exception import PizzeriaException
+from unittest.mock import Mock
+
 class TestCartePizzeria(unittest.TestCase): 
 
     #tester une carte vide
     def test_empty_carte_pizzeria(self):
         carte_pizz = CartePizzeria()
         self.assertTrue(carte_pizz.is_empty)
+
+    def test_pizza_number(self):
+        carte_pazz = CartePizzeria()
+        pizza = Mock()  
+        carte_pazz.add_pizza(pizza)
+        self.assertEqual(carte_pazz.nb_pizzas(), 1)
+
+    def test_remove_number(self): 
+        carte_pazz = CartePizzeria()
+        pizza = Mock()  
+        carte_pazz.add_pizza(pizza)
+        carte_pazz.remove_pizza(pizza)
+        self.assertEqual(carte_pazz.nb_pizzas(), 0)
+
+    def test_remove_pizza_not_found(self):
+        carte = CartePizzeria()
+        with self.assertRaises(PizzeriaException):
+            carte.remove_pizza("samy")
 
 
 
